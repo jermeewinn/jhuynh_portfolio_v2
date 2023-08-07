65cfeb9ef0807c19f4e7b8c84d061dcb39f24713
Jeremy Huynh <jeahuynh18@gmail.com>
2023-08-06 19:51:01 -0700

    further file setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from 'react';
+import Header from './components/Header/header.jsx';
+import Nav from './components/Nav/nav.jsx';
+import About from './components/About/About.jsx';
+import Experience from './components/Experience/experience.jsx';
+import Services from './components/Services/services.jsx';
+import Portfolio from './components/Portfolio/portfolio.jsx';
+import Testimonials from './components/Testimonials/testimonials.jsx';
+import Contact from './components/Contact/Contact.jsx';
+import Footer from './components/Footer/footer.jsx';
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+        <Header />
+        <Nav />
+        <About />
+        <Experience />
+        <Services />
+        <Portfolio />
+        <Testimonials />
+        <Contact />
+        <Footer />
+        
+    </>
   )
 }
 
