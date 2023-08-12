381218b29592f7d1cd6447eedbaee623b12da463
Jeremy Huynh <jeahuynh18@gmail.com>
2023-08-12 00:31:26 -0700

    added Linkedin & Github links for HeaderSocial subcomponent

--- src/components/Header/HeaderSocial.jsx
+++ src/components/Header/HeaderSocial.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import {BsLinkedin} from 'react-icons/bs';
-// import {BsGitHub} from 'react-icons/bs';
+import { BsLinkedin } from 'react-icons/bs';
+import { FaGithub } from 'react-icons/fa';
 
 const HeaderSocial = () => {
   return (
     <div className='header__social'>
       <a href='https://www.linkedin.com/in/jeremy-huynh/' target='_blank'><BsLinkedin /></a>
-      <a href='https://github.com/jermeewinn' target='_blank'></a>
-      <a href='https://www.linkedin.com/in/jeremy-huynh/' target='_blank'></a>
+      <a href='https://github.com/jermeewinn' target='_blank'><FaGithub /></a>
     </div>
   )
 }
